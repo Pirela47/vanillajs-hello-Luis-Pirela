247dc3408d306cbf00ffbed6bc797c79e6690d48
Pirela47 <luis.jpl294@gmail.com>
2024-09-25 04:58:07 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let NumberGenerator = () => {
+    let number = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+    let num = Math.floor(Math.random() * number.length);
+    return number[num];
+  };
+  console.log(NumberGenerator());
+
+  let pintaGenerator = () => {
+    let pinta = ["espada", "trebol", "corazon", "diamante"];
+    let num2 = Math.floor(Math.random() * pinta.length);
+    return pinta[num2];
+  };
+  console.log(pintaGenerator());
+
+  document.querySelector(".numero").innerHTML = NumberGenerator();
+  document.querySelector(".carta").classList.add(pintaGenerator());
 };
